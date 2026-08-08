586278b47cd56d445b4e98b307a5516f6f42e681
abh1nav24 <abhinavamoli24@gmail.com>
2026-08-08 23:05:15 +0530

    Create Q9.cpp

---
+++ Q9.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+class Area
+{
+public:
+
+    // Area of square
+    int calculateArea(int side)
+    {
+        return side * side;
+    }
+
+    // Area of rectangle
+    int calculateArea(int length, int breadth)
+    {
+        return length * breadth;
+    }
+
+    // Area of triangle
+    float calculateArea(float base, float height, int x)
+    {
+        return 0.5 * base * height;
+    }
+};
+
+int main()
+{
+    Area obj;
+
+    cout << "Area of Square: "
+         << obj.calculateArea(5) << endl;
+
+    cout << "Area of Rectangle: "
+         << obj.calculateArea(5, 10) << endl;
+
+    cout << "Area of Triangle: "
+         << obj.calculateArea(5.0f, 8.0f, 0) << endl;
+
+    return 0;
+}
